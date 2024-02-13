523abc552109036c82713afd2efb34a38b6eaa87
Daniel Leal <lealheda@gmail.com>
2024-02-12 21:23:44 -0700

    uuid element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroesModule, CounterModule],
+  imports: [RouterOutlet, HeroesModule, CounterModule, DbzModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
